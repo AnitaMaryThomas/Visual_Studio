b0b11c76845b92e908be6170de6a44b5a721f2ef
Anita Mary Thomas <anita.thomas@experionglobal.local>
2023-02-13 15:12:59 +0530

    first commit

---
+++ New_13/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace New_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swaping: a= " + a + " b= " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.Write("After swaping: a= " + a + " b= " + b);
+        }
+    }
+}
